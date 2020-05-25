954a0682ae53de9438dbbeb29ff5eb25108eaaf6
rajesh8871 <rajeshb421@gmail.com>
2020-05-25 12:17:20 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -60,4 +60,22 @@ $(document).ready(function() {
 
     navbarFixed();
 
+/*---------------------------------------------------- */
+  	/* Smooth Scrolling
+  	------------------------------------------------------ */
+  	$('.nav-link').on('click', function (e) {
+	 	
+	 	e.preventDefault();
+
+   	var target = this.hash,
+    	$target = $(target);
+
+    	$('html, body').stop().animate({
+       	'scrollTop': $target.offset().top
+      }, 1500, 'swing', function () {
+      	window.location.hash = target;
+      });
+
+  	});  
+
 });
